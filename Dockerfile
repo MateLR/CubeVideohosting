6f9bae01ea2026b4f2637819046a2f00ead575b7
MateLR <utenkov2003@mail.ru>
2023-06-13 17:32:26 +0500

    Add docker and change db.sqlite to postgresql

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.9
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR /usr/src/cube
+
+COPY ./req.txt /usr/src/req.txt
+RUN pip install --upgrade pip && pip install -r /usr/src/req.txt
+
+COPY . /usr/src/cube
+
+#EXPOSE 8000
+#CMD ["python", "manage.py", "migrate"]
+#CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- cube/settings.py
+++ cube/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from pathlib import Path
 from .keys import *
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +27,7 @@ SECRET_KEY = django_secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -76,13 +77,34 @@ WSGI_APPLICATION = 'cube.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# local database with postgres
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'cube',
+#         'USER': 'postgres',
+#         'PASSWORD': 'ruslik',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432'
+#     }
+# }
+#docker database with postgres
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'cube_db',
+        'PORT': '5432'
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
